8ed2b772950f2d05450ead472212a5e1e1807f0a
Ikkodo-Kamoi <kamoih02@gmail.com>
2022-08-30 13:47:14 +0900

    add new django settings

--- app/config/settings/develop.py
+++ app/config/settings/develop.py
@@ -1,3 +1,14 @@
 from .base import *
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
+    }
+}
